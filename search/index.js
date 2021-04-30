e52f03f174ce3aee58d1e08ba0518fb63d6132ed
Ande Caleb <andaeiii@aol.com>
2021-04-30 11:43:46 +0100

    little updates...

--- search/index.js
+++ search/index.js
@@ -1,5 +1,6 @@
 require ('dotenv').config();
 const { google } = require('googleapis');
+const { axios } = require('../node_modules/axios/dist/axios');
 //console.log(process.env.YOUTUBE_TOKEN);
 
 
@@ -10,14 +11,31 @@ google.youtube('v3').search.list({
     q: 'Next Level',
     channelId: 'UCALuqr8BQi_djTbgEL5yeGA'
 }).then(response => {                   //returns a promise... 
-    console.log(response);
+   // console.log(response);
 
     const { data } = response;  //destructuring...
 
     data.items.forEach(item => {
-        console.log(`Title: ${item.snippet.title} \n Description: ${item.snippet.description} \n`)
+       // console.log(`Title: ${item.snippet.title} \n Description: ${item.snippet.description} \n`)
     });
 
 }).catch(errors => {
-    console.log(errors);
+   // console.log(errors);
+});
+
+const params = {
+    part: 'snippet',
+    maxResults: '40',
+    key: 'AIzaSyDFyn5GcJ2KdSiPcCV-41rdi5FvNrDOems',
+    q: 'next level',
+    channelId:'UCALuqr8BQi_djTbgEL5yeGA'
+};
+
+axios.get('https://youtube.googleapis.com/youtube/v3/search', params)
+.then(res => {
+    console.log('exxysy...');
+    console.log(response);
 })
+.catch(err => {
+    console.log(response);
+});
