5b405dc357e9f55f1b3909ceadadfee2f7ba0e98
Ande Caleb <andaeiii@aol.com>
2021-05-05 08:41:54 +0100

    using the node-fetch api

--- search/index.js
+++ search/index.js
@@ -1,6 +1,6 @@
-require ('dotenv').config();
+require('dotenv').config();
 const { google } = require('googleapis');
-const { axios } = require('../node_modules/axios/dist/axios');
+const fetch = require('node-fetch');
 //console.log(process.env.YOUTUBE_TOKEN);
 
 
@@ -11,31 +11,33 @@ google.youtube('v3').search.list({
     q: 'Next Level',
     channelId: 'UCALuqr8BQi_djTbgEL5yeGA'
 }).then(response => {                   //returns a promise... 
-   // console.log(response);
+    // console.log(response);
 
     const { data } = response;  //destructuring...
 
     data.items.forEach(item => {
-       // console.log(`Title: ${item.snippet.title} \n Description: ${item.snippet.description} \n`)
+        console.log(`Title: ${item.snippet.title} \n Description: ${item.snippet.description} \n`)
     });
 
+    //console.log(data.items)
+
 }).catch(errors => {
-   // console.log(errors);
+    // console.log(errors);
 });
+
+
+//using the fetch api... 
 
 const params = {
     part: 'snippet',
     maxResults: '40',
-    key: 'AIzaSyDFyn5GcJ2KdSiPcCV-41rdi5FvNrDOems',
+    key: process.env.YOUTUBE_TOKEN,
     q: 'next level',
-    channelId:'UCALuqr8BQi_djTbgEL5yeGA'
+    channelId: 'UCALuqr8BQi_djTbgEL5yeGA'
 };
 
-axios.get('https://youtube.googleapis.com/youtube/v3/search', params)
-.then(res => {
-    console.log('exxysy...');
-    console.log(response);
-})
-.catch(err => {
-    console.log(response);
-});
+fetch('https://youtube.googleapis.com/youtube/v3/search', params)
+    .then(res => res.text())
+    .then(body => console.log(body));
+
+
